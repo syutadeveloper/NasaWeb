7deec69489285402c1a7cba83a22c33d7b1074d9
Moretty <142655155+ivan78789@users.noreply.github.com>
2025-10-04 08:56:23 +0600

    new: обновил страницы

---
+++ layout/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($TitleName)?></title>
+</head>
+<body>
+    <div class="wrapper">
+
--- views/index.php
+++ views/index.php
@@ -18,3 +18,11 @@ require_once __DIR__ . '/../config/db.php';
 
 
 ?>
+
+<?php require_once __DIR__ . '/../layout/header.php' ?>
+<?php require_once __DIR__ . '/../layout/nav.php' ?>
+<h1>  Привет это главная страница </h1>
+
+<?php require_once __DIR__ . '/../layout/footer.php' ?>
+
+
